d8a3c114d75f4e703ee9be19d2f51b42c2241891
gsinthujan@yahoo.com <gsinthujan@yahoo.com>
2016-09-06 01:15:50 +0530

    Base Commit

---
+++ src/main/webapp/static/js/controller/user_controller.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('myApp').controller('UserController', ['$scope', 'SmartLadyService', function($scope, SmartLadyService) {
+   
+}]);
---
+++ src/main/webapp/static/js/service/user_service.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('myApp').factory('SmartLadyService', ['$http', '$q', function($http, $q){
+
+    
+}]);
